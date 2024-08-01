0b3e06274044260adbef372692deab5988b52305
fanyang <1776336252@qq.com>
2024-08-01 14:17:24 +0800

    feat: 修改loading文案

--- src/layouts/DocLayout.tsx
+++ src/layouts/DocLayout.tsx
@@ -12,18 +12,22 @@ import '../slots/_.less';
  */
 export default () => {
   const { themeConfig, loading } = useSiteData();
-  const {
-    navs
-  } = themeConfig;
-
-
+  const { navs } = themeConfig;
 
   // 打印控制台文案
   useEffect(() => {
-    console.log("%cLogicFlow 让流程图绘制更方便快捷", "color:#5B7102; font-size: 20px;")
-    console.log("%c打造新一代流程图绘制和执行的解决方案", "color:#5B7102;")
-    console.log("--------------------------")
-    console.log("%c添加我们的官方微信号 %c“logic-flow”%c，申请加入 LogicFlow 用户群讨论问题，也欢迎加入我们共建项目！", "color: red", "color: pink", "color: red")
+    console.log(
+      '%cLogicFlow 让流程图绘制更方便快捷',
+      'color:#5B7102; font-size: 20px;',
+    );
+    console.log('%c打造新一代流程图绘制和执行的解决方案', 'color:#5B7102;');
+    console.log('--------------------------');
+    console.log(
+      '%c添加我们的官方微信号 %c“logic-flow”%c，申请加入 LogicFlow 用户群讨论问题，也欢迎加入我们共建项目！',
+      'color: red',
+      'color: pink',
+      'color: red',
+    );
   }, []);
 
   const outlet = useOutlet();
@@ -41,20 +45,25 @@ export default () => {
     }
   }, [loading, hash]);
 
-  const path = pathname
+  const path = pathname;
   // 统一去掉中英文前缀
   let p = path.replace('/zh/', '/').replace('/en/', '/');
   // 首页
   if (p === '/' || p === '/zh' || p === '/en' || p === '/en/') return <Index />;
 
   // 匹配 navs 中的 docs 路由
-  const docsRoutes =
-    navs
-    .filter(nav => nav.slug && nav.slug.startsWith('docs/'))
-    .map(nav => nav.slug && nav.slug.split('/').find(item => item !== 'docs'));
+  const docsRoutes = navs
+    .filter((nav) => nav.slug && nav.slug.startsWith('docs/'))
+    .map(
+      (nav) => nav.slug && nav.slug.split('/').find((item) => item !== 'docs'),
+    );
 
-  if (docsRoutes.some(route => p.startsWith(`/${route}`) || p.startsWith(`/docs/${route}`))) {
-    return <Manual>{outlet}</Manual>
+  if (
+    docsRoutes.some(
+      (route) => p.startsWith(`/${route}`) || p.startsWith(`/docs/${route}`),
+    )
+  ) {
+    return <Manual>{outlet}</Manual>;
   }
 
   return outlet;
--- src/slots/Loading/index.tsx
+++ src/slots/Loading/index.tsx
@@ -4,22 +4,30 @@ import styles from './index.module.less';
 /**
  * Loading
  */
-const Loading = () => (
-  <div
-    style={{
-      position: 'relative',
-      height: '100%',
-      width: '100%',
-    }}
-  >
-    <div className={styles.loading}>
-      <div className="container">
-        <div className="loading-text">
-          <p>Loading...</p>
+const Loading = () => {
+  return (
+    <div
+      style={{
+        position: 'relative',
+        height: '100%',
+        width: '100%',
+      }}
+    >
+      <div className={styles.loading}>
+        <div className="container">
+          <div className="loading-icon">
+            <img
+              src="	https://cdn.jsdelivr.net/gh/Logic-Flow/static@latest/docs/logo-horizontal-blue.png"
+              alt="LogicFlow"
+            />
+          </div>
+          <div className="loading-text">
+            <p>Loading...</p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Loading;
